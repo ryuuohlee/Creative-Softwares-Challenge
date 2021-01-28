b32666a5c50f3e6267babe2879e535f1f0db2f4d
Jeffrey Lee <jglee1@live.com>
2021-01-28 14:04:20 -0800

    Feature: started task list

---
+++ to-do/src/components/TaskList/TaskList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const TaskList = (props) => {
+  console.log(props)
+    return (
+      <div className="openTasks">
+        tasks
+      </div>
+    );
+}
+
+export default TaskList;
--- to-do/src/container/App/App.js
+++ to-do/src/container/App/App.js
@@ -1,12 +1,14 @@
 import React, { useState } from 'react';
 import CurrentDate from '../../components/CurrentDate/CurrentDate.js';
 import OpenTasks from '../../components/OpenTasks/OpenTasks.js';
+import TaskList from '../../components/TaskList/TaskList.js'
 
 
 const App = () => {
   const date = new Date();
   const dateParse = date.toString().split(' ');
   const [openTasks, setOpenTasks] = useState(0)
+  const [taskList, setTasks] = useState([{task:'task 1', priority:'Low', date:'Nov 7th, 2020', status: false}, {task:'task 2', priority:'Medium', date:'Nov 8th, 2020', status: false}, {task:'task 3', priority:'high', date:'Nov 9th, 2020', status: true}])
 
   console.log(dateParse)
 
@@ -14,9 +16,7 @@ const App = () => {
       <div className="App">
         <CurrentDate date={dateParse} />
         <OpenTasks open={openTasks} />
-        {/*
-        <TaskList />
-        <Modal> */}
+        <TaskList tasks={taskList} />
       </div>
     );
 }
